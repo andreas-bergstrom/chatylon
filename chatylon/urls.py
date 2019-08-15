3e99500a75f9025640a1ad2cef9ca7503dcfe4c1
Andreas Bergström <andreas.bergstrom@gmail.com>
2019-08-15 13:01:45 +0200

    Add / redirect to /threads

--- chatylon/urls.py
+++ chatylon/urls.py
@@ -1,36 +1,22 @@
-"""chatylon URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-import django.urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 import chat
 from chatylon import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', django.urls.include('django.contrib.auth.urls')),
-    path('accounts/', django.urls.include('accounts.urls')),
-    path('threads/', django.urls.include('chat.urls', namespace='chat')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('threads/', include('chat.urls', namespace='chat')),
+    path('', RedirectView.as_view(url=reverse_lazy('chat:threads'), permanent=True))
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('__debug__/', django.urls.include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
 
     ] + urlpatterns
 
