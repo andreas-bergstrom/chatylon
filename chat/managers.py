d987bbc226928f63de0283923932de9b83dce0a7
Andreas Bergström <andreas.bergstrom@gmail.com>
2019-08-31 08:33:18 +0200

    Optimize queryset

--- chat/managers.py
+++ chat/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class MessageQuerySet(models.QuerySet):
-    def messages_in_thread(self, thread):
-        return self.model.objects.filter(thread=thread).order_by('created').prefetch_related('sender')
+    def messages_in_thread(self, thread, *args, **kwargs):
+        return self.filter(thread=thread, *args, **kwargs).order_by('created').prefetch_related('sender')
